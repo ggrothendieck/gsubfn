7bdda8970c3601ed9789dbe199b6dc5ea686a9fc
ggrothendieck <ggrothendieck@16f98506-a41e-0410-a92f-9d946f01d26a>
2006-10-03 02:21:13 +0000

    Added $.fn

---
+++ R/fn.R
@@ -0,0 +1,58 @@
+
+as.function.formula <- function(x, ...) {
+	vars <- all.vars(x[[2]])
+	if (length(vars) == 0) { 
+		f <- function() {}
+	} else {
+		f <- function(x) {}
+		formals(f) <- rep(formals(f), length(vars))
+		names(formals(f)) <- vars
+	}
+	body(f) <- x[[length(x)]]
+	environment(f) <- environment(x)
+	f
+}
+
+match.funfn <- function(x, ...) UseMethod("match.funfn")
+match.funfn.default <- base::match.fun
+match.funfn.formula <- as.function.formula
+
+fn <- structure(NA, class = "fn")
+"$.fn" <- function(x, FUN) {
+	FUN <- match.fun(FUN)
+
+	function(...) {
+		args <- list(...)
+		mc <- if (is.primitive(FUN)) match.call()[-1]
+		else match.call(FUN)[-1]
+		nm <- names(mc)
+		if (is.null(nm)) nm <- rep("", length(args))
+		idx <- match("simplify", tolower(nm), nomatch = 0)
+		simplify <- NULL
+		if (idx > 0) {
+			if (!is.logical(args[[idx]])) {
+				simplify <- args[[idx]]
+				args <- args[-idx]
+				nm <- nm[-idx]
+			}
+		}
+		is.fo <- sapply(args, function(x) is(x, "formula"))
+		num.fo <- sum(is.fo)
+	
+		for(i in seq(along = args)) {
+		   if (is.fo[i] && (num.fo == 1 || i > 1 || nm[[i]] == "FUN"))
+		         args[[i]] <- as.function(args[[i]])
+		}
+		out <- do.call(FUN, args)
+		if (!is.null(simplify)) {
+			if(!is.list(out)) out <- list(out) 
+			out <- do.call(simplify, out)
+		}
+		out
+	}
+}
+		
+# test
+# fn$list(x ~ 2*x)
+# fn$mapply(~ x + y, 1:10, 21:30)
+
