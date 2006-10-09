b7ac763cb5eb8d446f6dc6597d220408c3986a55
ggrothendieck <ggrothendieck@16f98506-a41e-0410-a92f-9d946f01d26a>
2006-10-09 02:49:20 +0000

    more gries.R tweaks

--- demo/gries.R
+++ demo/gries.R
@@ -7,7 +7,8 @@ fn1 <- system.file("COPYING", package = "gsubfn")
 Lines1 <- tolower(scan(fn1, what = "char", sep = "\n"))
 print(tail(sort(table(unlist(strapply(Lines1, "\\w+", perl = TRUE))))))
 
-# frequency list of words from an SGML-annotated text file
+# frequency list of words from an SGML-annotated text file 
+# sampled from the British National Corpus"
 
 fn2 <- system.file("sample.txt", package = "gsubfn")
 Lines2 <- scan(fn2, what = "char", sep = "\n")
@@ -22,6 +23,7 @@ print(tail(words, 25))
 
 word.tag.pairs <- unlist(strapply(tagged.corpus.sentences, "<[^<]*")) 
 cleaned.word.tag.pairs <- grep("<w ", word.tag.pairs, value = TRUE)
+cleaned.word.tag.pairs <- gsub(" +$", "", cleaned.word.tag.pairs)
 print(tail(sort(table(cleaned.word.tag.pairs))))
 print(tail(cleaned.word.tag.pairs))
 
