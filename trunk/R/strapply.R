24be7278a2189601c8d72bf603bfec4ae2f4a6ad
ggrothendieck <ggrothendieck@16f98506-a41e-0410-a92f-9d946f01d26a>
2006-10-10 09:33:48 +0000

    first modification toward 0.2-2.  combine= arg added to strapply.

--- trunk/R/strapply.R
+++ trunk/R/strapply.R
@@ -8,20 +8,24 @@
 #	  }, simplify = simplify, USE.NAMES = USE.NAMES)
 
 
+
 strapply <- 
 function (X, pattern, FUN = function(x, ...) x, ..., simplify = FALSE, 
-    USE.NAMES = FALSE) {
-	# if (inherits(FUN, "formula")) FUN <- as.function(FUN)
-	FUN <- match.funfn(FUN)
-	result <- sapply(X, function(x) {
-	    first <- TRUE
-	    v <- NULL
-	    gsubfn(pattern, function(x, ...)
-		v <<- if (first) { first <<- FALSE; FUN(x, ...) } 
-		else c(v, FUN(x, ...)),
-		x, ...)
-	    v
-	}, simplify = is.logical(simplify) && simplify, USE.NAMES = USE.NAMES)
-	if (is.logical(simplify)) result else do.call(simplify, result)
+    USE.NAMES = FALSE, combine = c) 
+{
+    FUN <- match.funfn(FUN)
+    result <- sapply(X, function(x) {
+        first <- TRUE
+        v <- NULL
+        gsubfn(pattern, function(x, ...) v <<- if (first) {
+            first <<- FALSE
+            FUN(x, ...)
+        }
+        else combine(v, FUN(x, ...)), x, ...)
+        v
+    }, simplify = is.logical(simplify) && simplify, USE.NAMES = USE.NAMES)
+    if (is.logical(simplify)) 
+        result
+    else do.call(simplify, result)
 }
 
