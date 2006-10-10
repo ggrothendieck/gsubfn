3c0cf70741f7c0d76d17c7534923f6d8c05728ee
ggrothendieck <ggrothendieck@16f98506-a41e-0410-a92f-9d946f01d26a>
2006-10-10 00:14:17 +0000

    add chron.R demo

---
+++ demo/chron.R
@@ -0,0 +1,22 @@
+
+# Use read.zoo to read data with a chron time index
+# Ignore fractional seconds.
+
+library(zoo)
+library(chron)
+library(gsubfn)
+
+# test data
+Lines <- "2006-01-24 02:41:24.00011,1.22930000,5,1.22950000,7
+2006-01-25 04:41:24.00011,1.22930000,5,1.22950000,7
+2006-01-26 07:41:24.00011,1.22930000,5,1.22950000,7"
+
+# convert to chron
+to.chron <- function(x) 
+   strapply(format(x), "([0-9-]+) ([0-9:]+)",
+	~ chron(as.Date(dd), tt), backref = -2,simplify = c)
+
+con <- textConnection(Lines)
+read.zoo(con, sep = ",", FUN = to.chron)
+close(con)
+
