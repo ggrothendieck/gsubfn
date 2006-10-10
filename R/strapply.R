cda24a2c02e96ad321de457ea1de67151ac2cfd6
ggrothendieck <ggrothendieck@16f98506-a41e-0410-a92f-9d946f01d26a>
2006-10-10 10:59:35 +0000

    strapply improvements

--- R/strapply.R
+++ R/strapply.R
@@ -1,31 +1,15 @@
-
-#strapply <- function(X, pattern, FUN = function(x, ...) x, ..., 
-#   simplify = FALSE, USE.NAMES = FALSE)
-#      sapply(X, function(x) {
-#          v <- NULL
-#	  gsubfn(pattern, function(x, ...) v <<- c(v, FUN(x, ...)), x, ...)
-#	  v
-#	  }, simplify = simplify, USE.NAMES = USE.NAMES)
-
-
-
 strapply <- 
 function (X, pattern, FUN = function(x, ...) x, ..., simplify = FALSE, 
     USE.NAMES = FALSE, combine = c) 
 {
     FUN <- match.funfn(FUN)
     result <- sapply(X, function(x) {
-        first <- TRUE
         v <- NULL
-        gsubfn(pattern, function(x, ...) v <<- if (first) {
-            first <<- FALSE
-            FUN(x, ...)
-        }
-        else combine(v, FUN(x, ...)), x, ...)
-        v
+        gsubfn(pattern, function(x, ...) 
+		v <<- c(v, combine(FUN(x, ...))), x, ...)
+	v
     }, simplify = is.logical(simplify) && simplify, USE.NAMES = USE.NAMES)
     if (is.logical(simplify)) 
         result
     else do.call(simplify, result)
 }
-
