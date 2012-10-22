75e87e2b00de6bac1fdce6e23a7d6c8f73bb3c00
ggrothendieck@gmail.com <ggrothendieck@gmail.com@16f98506-a41e-0410-a92f-9d946f01d26a>
2012-10-22 15:43:50 +0000

    removed Windows vignettes menu

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,6 @@
 
 .onLoad <- function(libname, pkgname) {
 
-	if (.Platform$OS.type == "windows" && 
-		interactive() && 
-		!isTRUE(getOption("gsubfn.noVignetteMenu")) && 
-		file.exists(system.file(package = "Zelig")) &&
-		file.exists(system.file("Meta", "vignette.rds", package = pkgname))) {
-		  Zelig:::addVigs2WinMenu(pkgname)
-	}
-
 	gsubfn.engine <- getOption("gsubfn.engine")
 	# if gsubfn.engine was not set to "R" then check if tcltk can be used
     if ( ! identical(gsubfn.engine, "R") ) {
